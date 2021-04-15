344786e3b6d86a9953413207212ad5f7f8960aed
Trinh_Vo <trinh.vo@synolia.com>
2021-04-15 15:51:25 +0200

    use Symfony Cache in getAccount()

--- src/Gateway/Validator/Constraints/IsPayPlugSecretKeyValidator.php
+++ src/Gateway/Validator/Constraints/IsPayPlugSecretKeyValidator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PayPlug\SyliusPayPlugPlugin\Gateway\Validator\Constraints;
 
 use Payplug\Exception\UnauthorizedException;
+use PayPlug\SyliusPayPlugPlugin\ApiClient\PayPlugApiClientFactory;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
@@ -15,6 +16,14 @@ use Symfony\Component\Validator\Exception\UnexpectedValueException;
  */
 final class IsPayPlugSecretKeyValidator extends ConstraintValidator
 {
+    /** @var PayPlugApiClientFactory */
+    private $apiClientFactory;
+
+    public function __construct(PayPlugApiClientFactory $apiClientFactory)
+    {
+        $this->apiClientFactory = $apiClientFactory;
+    }
+
     public function validate($value, Constraint $constraint): void
     {
         if (!$constraint instanceof IsPayPlugSecretKeyValid) {
@@ -30,6 +39,8 @@ final class IsPayPlugSecretKeyValidator extends ConstraintValidator
         try {
             \Payplug\Payplug::init(['secretKey' => $value]);
             \Payplug\Authentication::getPermissions();
+            $apiClient = $this->apiClientFactory->create('payplug_oney', $value);
+            $apiClient->getAccount(true);
         } catch (UnauthorizedException $exception) {
             $this->context->buildViolation($constraint->message)
                 ->addViolation();
